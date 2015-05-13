ee6aec2e8d826228791965242942616814fe271d
Graham Clenaghan <clenagh@math.washington.edu>
2015-05-12 20:39:27 -0700

    Import colors from python code

--- sieve.js
+++ sieve.js
@@ -1,1 +1,6 @@
-//Javascript goes here
+var aacolor = d3.scale.ordinal()
+	.range(['#CCFF00','#FFFF00','#FF0000','#FF0066','#00FF66','#FF9900','#0066FF',
+		'#66FF00','#6600FF','#33FF00','#00FF00','#CC00FF','#FFCC00','#FF00CC',
+		'#0000FF','#FF3300','#FF6600','#99FF00','#00CCFF','#00FFCC'])
+	.domain(['A','C','D','E','F','G','H','I','K','L','M',
+		'N','P','Q','R','S','T','V','W','Y']);
