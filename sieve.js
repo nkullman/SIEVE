fd2eede34c6854eb7251f81e757708bc45b02a11
Nick Kullman <Nick.Kullman@gmail.com>
2015-05-16 11:01:24 -0700

    Added xScale, yScale, zoom, transform, drawing of rectangles

--- sieve.js
+++ sieve.js
@@ -24,36 +24,59 @@ function generateSiteSelector() {
 	  ];
 	});
 	
-	var xScale = d3.scale.linear()
+	//
+	var xScale = d3.scale.ordinal()
+		.domain(d3.range(vaccine.sequence.length))
+		.rangeBands([0,width], 0.05);
+		
+	var yScale = d3.scale.linear()
+		.domain([0, 1])
+		.range([height, 0]);
+	/*
+	var exScale = d3.scale.linear()
 	    .domain([0, width])
 	    .range([0, width]);
 	
-	var yScale = d3.scale.linear()
+	var eyScale = d3.scale.linear()
 	    .domain([0, height])
-	    .range([height, 0]);
+	    .range([height, 0]);*/
 	
 	var svg = d3.select("#overview").append("svg")
 	    .attr("width", width)
 	    .attr("height", height)
 	  .append("g")
-	    .call(d3.behavior.zoom().x(xScale).y(yScale).scaleExtent([1, 8]).on("zoom", zoom));
+	    .call(d3.behavior.zoom().x(xScale).y(eyScale).scaleExtent([1, 8]).on("zoom", zoom));
 	
 	svg.append("rect")
 	    .attr("class", "overlay")
 	    .attr("width", width)
 	    .attr("height", height);
 	
-	var circle = svg.selectAll("circle")
+	/*var circle = svg.selectAll("circle")
 	    .data(data)
 	  .enter().append("circle")
 	    .attr("r", 2.5)
-	    .attr("transform", transform);
+	    .attr("transform", transform);*/
+		
+	var rectangle = svg.selectAll("rect")
+		.data(vaccine.sequence)
+		.enter().append("rect")
+		  .attr("x", function (d,i) { return xScale(i); })
+		  .attr("y", yScale(0) );
 	
-	function zoom() {
+	/*function zoom() {
 	  circle.attr("transform", transform);
 	}
 	
 	function transform(d) {
-	  return "translate(" + xScale(d[0]) + "," + yScale(d[1]) + ")";
+	  return "translate(" + exScale(d[0]) + "," + eyScale(d[1]) + ")";
+	}*/
+	
+	function zoom() {
+	  rectangle.attr("transform", transform);
+	}
+	
+	function transform(d, i) {
+	  return "translate(" + xScale(i) + "," + yScale(0) + ")";
 	}
 }
