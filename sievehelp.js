dd06d85b93ef0caca8884252f1a1c1d21f1a1d88
Nick Kullman <Nick.Kullman@gmail.com>
2015-05-31 15:24:14 -0700

    Add slightly more relevant content to the help div

--- sievehelp.js
+++ sievehelp.js
@@ -4,7 +4,8 @@ var sievehelpdiv = d3.select(".title").append("div")
 	.classed("hidden", true);
 
 // add div content
-sievehelpdiv.append("h1").text("This is a big title!");
+sievehelpdiv.append("h1").text("How to use this tool");
+sievehelpdiv.append("p").text("Select sites in the shaded area.");
 
 // hit window is the 'help' text
 // set its behavior for mouseover and mouseout
