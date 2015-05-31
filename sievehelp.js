a77587eb120ccbbdb049e75e5e7eee65962d40cb
Nick Kullman <Nick.Kullman@gmail.com>
2015-05-31 15:56:46 -0700

    Added more content to help div

--- sievehelp.js
+++ sievehelp.js
@@ -4,8 +4,17 @@ var sievehelpdiv = d3.select(".title").append("div")
 	.classed("hidden", true);
 
 // add div content
-sievehelpdiv.append("h1").text("How to use this tool");
-sievehelpdiv.append("p").text("Select sites in the shaded area.");
+sievehelpdiv.append("h3").text("How to use this tool");
+sievehelpdiv.append("p").html(
+	"<ul>"+
+		"<li>Select sites from the vaccine sequence in the shaded region.</li>" +
+		"<li>Scroll or double click to zoom through the sequence.</li>" +
+		"<li>Click and drag to pan</li>" +
+		"<li>To select sites, hold down shift and click and drag</li>" +    
+		"<li>The detailed charts will update as you change your selection.</li>" +
+	"</ul>"
+);
+
 
 // hit window is the 'help' text
 // set its behavior for mouseover and mouseout
