cbf52fbd355add0f8c54b7774fe6737a8552615b
Wayne Yang <wfyang@uw.edu>
2015-06-04 14:32:55 -0700

    add adjusted pvalues (script)

--- pvalues.R
+++ pvalues.R
@@ -8,10 +8,19 @@ thedata = merge(mismatches,treatment,by="sampleID")[-1]
 
 pvalues = c()
 for(i in 1:(ncol(thedata)-1)){
+  print(i)
   pvalues = c(pvalues,perm.t.test(thedata[,i][(thedata[,1012] == "VACCINE")]
                                   ,thedata[,i][(thedata[,1012] == "PLACEBO")]
                                   ,alternative = "two.sided",B=10000)$p.value)
 }
+
+qvalues = p.adjust(pvalues,method = "fdr")
+for(i in 1:length(qvalues)){
+  if(is.na(qvalues[i])){
+    qvalues[i] = 1
+  }
+}
+
 
 for(i in 1:length(pvalues)){
   if(is.na(pvalues[i])){
@@ -19,7 +28,10 @@ for(i in 1:length(pvalues)){
   }
 }
 
-temp = data.frame(pvalues)
-names(temp)[1] = "pvalue"
 
+temp = data.frame(pvalues)
+temp2 = data.frame(qvalues)
+names(temp)[1] = "pvalue"
+names(temp2)[1] = "qvalue"
 write.csv(temp,"pvalues.csv",row.names=F)
+write.csv(temp2,"qvalues.csv",row.names=F)
